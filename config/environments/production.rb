4c0880b6e5074b54cfca379183121afea72b235b
lan <nguyen.phuong.lan@framgia.com>
2015-06-26 10:45:39 +0700

    Demo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://dry-caverns-9616.herokuapp.com'
+  host = 'https://demolog.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
